34e9a2111476306f51c47b75ee8c48d98a1a8e8a
Gold3nEagle <g3agletech@gmail.com>
2025-01-21 14:45:00 +0300

    redesigned the book  to contain flip page buttons and pdf download icon, also created main menu script to handle opening the link to download the pdf.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void OpenPDFDownloadLink()
+    {
+        Application.OpenURL("https://drive.google.com/file/d/1wMidriizFvQdixI76MhWhimog9OPupnG/view");
+    }
+
+
+}
